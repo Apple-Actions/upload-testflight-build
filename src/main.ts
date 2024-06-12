f8e64b0f58f6925ec6883f12033d01c7c09842da
Skandar Munir <skandar_munir@yahoo.com>
2024-06-12 16:56:02 +0300

    retry upload if timeout (#1)
    feature add: retry on timeout for upload

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
 import * as os from 'os'
 import * as altool from './altool'
-
+import {retry} from 'ts-retry-promise'
 import {ExecOptions} from '@actions/exec/lib/interfaces'
 
 async function run(): Promise<void> {
@@ -15,6 +15,9 @@ async function run(): Promise<void> {
     const apiPrivateKey: string = core.getInput('api-private-key')
     const appPath: string = core.getInput('app-path')
     const appType: string = core.getInput('app-type')
+    const retryAttempts: number = parseInt(
+      core.getInput('retry-attempts-on-timeout')
+    )
 
     let output = ''
     const options: ExecOptions = {}
@@ -25,9 +28,35 @@ async function run(): Promise<void> {
     }
 
     await altool.installPrivateKey(apiKeyId, apiPrivateKey)
-    await altool.uploadApp(appPath, appType, apiKeyId, issuerId, options)
-    await altool.deleteAllPrivateKeys()
 
+    const uploadWithRetry = async (): Promise<void> => {
+      try {
+        await altool.uploadApp(appPath, appType, apiKeyId, issuerId, options)
+      } catch (e) {
+        if (output.includes('The request timed out')) {
+          throw Error('timeout')
+        }
+
+        throw e
+      }
+    }
+
+    try {
+      await retry(uploadWithRetry, {
+        retries: retryAttempts,
+        delay: 2000,
+        retryIf(error) {
+          return error.message === 'timeout'
+        }
+      })
+    } catch (error) {
+      core.warning(
+        `Upload failed after ${retryAttempts} attempts: ${error.message}`
+      )
+      throw error
+    }
+
+    await altool.deleteAllPrivateKeys()
     core.setOutput('altool-response', output)
   } catch (error) {
     core.setFailed(error.message)
